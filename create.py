f16b3cce0f9b9bdc6a0108ceb116d74b069c6b85
kitamas <kitamas25@gmail.com>
2022-09-22 11:55:47 +0200

    commit

--- create.py
+++ create.py
@@ -78,7 +78,8 @@ def webhook():
 
 @app.route('/createEvent', methods=['GET','POST'])
 def createEvent(service, minTime, maxTime):
-    #req = request.get_json(force=True)
+    req = request.get_json(force=True)
+    print(req)
     #session_name = req.get('sessionInfo').get('session')
     session_name = "AAA"
     try:
