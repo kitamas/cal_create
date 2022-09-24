eb644c33eb9f781819ceffe992ecf1423ccb5605
kitamas <kitamas25@gmail.com>
2022-09-24 10:14:46 +0200

    commit

--- create.py
+++ create.py
@@ -147,8 +147,8 @@ def main():
             cstTimeNow = dateTime.replace(tzinfo=tzObject)
             start = cstTimeNow.isoformat("T", "seconds")
 
-            #dt = datetime.datetime(2022, 09, 10, 01, 48, 34, 01)
-            dt = datetime.datetime(2022, 09, 10, 01, 48, 34)
+            dt = datetime.datetime(2022, 09, 10, 01, 48, 34, 01)
+            #dt = datetime.datetime(2022, 09, 10, 01, 48, 34)
             #d = dt.isoformat("T", "seconds")
             #print('Input Datetime string to ISO 8601 format:', d)
 
