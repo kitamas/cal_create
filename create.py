d51fcd080c3ba4c1eed3105680db387fbcf59d49
kitamas <kitamas25@gmail.com>
2022-09-25 16:36:26 +0200

    commit

--- create.py
+++ create.py
@@ -113,25 +113,15 @@ def main():
     d = datetime.datetime.now().date()
     today = datetime.datetime(d.year, d.month, d.day, 10)+datetime.timedelta(hours=1)
     #start = today.isoformat("T", "seconds")
-    #start = dt.isoformat("T", "seconds")
-
-    print("START")
-    print(start)
-
-    #print("DSTART")
-    #print(dstart)
 
     #end = (today + datetime.timedelta(hours=1)).isoformat("T", "seconds")
 
-    print("END")
-    print(end)
-
     start = datetime.datetime(2022, 9, 25, 16, 30, 0)
     end = start_time + datetime.timedelta(hours=1)
-    print("START start_time")
-    print(start_time)
-    print("END end_time")
-    print(end_time)
+    print("START")
+    print(start)
+    print("END")
+    print(end)
 
     event_result = service.events().insert(calendarId='61u5i3fkss34a4t50vr1j5l7e4@group.calendar.google.com',
        body={
