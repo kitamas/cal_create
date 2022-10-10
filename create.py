d5b683f4cf4c04257f5eeb9f8b807804a3490c8e
kitamas <kitamas25@gmail.com>
2022-10-10 17:30:24 +0200

    commit

--- create.py
+++ create.py
@@ -173,8 +173,8 @@ def hour_rounder(t):
     return (t.replace(second=0, microsecond=0, minute=0, hour=t.hour) + datetime.timedelta(hours=t.minute // 30))
 
 def check_open():
-    # current_dateTime = datetime.datetime.now() + datetime.timedelta(hours=3)
-    current_dateTime = datetime.datetime.now()
+    current_dateTime = datetime.datetime.now() + datetime.timedelta(hours=3)
+    # current_dateTime = datetime.datetime.now()
     # 2022-10-07 16:03:58.003427
 
     current_dateTime_rounded = hour_rounder(current_dateTime)
@@ -198,15 +198,15 @@ def check_open():
 
     if hour_minute < open_start_time[week_day]:
         print("KORÁN", hour_minute, "<", open_start_time[week_day])
-        text = "KORÁN. A mai nyitás " + open_start_time[week_day] + " a zárás " + open_end_time[week_day]
+        text_check_open = "KORÁN. A mai nyitás " + open_start_time[week_day] + " a zárás " + open_end_time[week_day]
 
     if hour_minute >= open_end_time[week_day]:
         print("KÉSŐN", hour_minute, ">=", open_end_time[week_day])
-        text = "KÉSŐN. A mai nyitás " + open_start_time[week_day] + " a zárás " + open_end_time[week_day]
+        text_check_open = "KÉSŐN. A mai nyitás " + open_start_time[week_day] + " a zárás " + open_end_time[week_day]
 
     if hour_minute >= open_start_time[week_day] and hour_minute <= open_end_time[week_day]:
         print("KOZOTTE", open_start_time[week_day], "<", hour_minute, "<", open_end_time[week_day])
-        text = "True"
+        text_check_open = "True"
 
     # return checked_open
     return text
