838baa49b3d7f3cead6dcc888f8d8870c7aaa071
kitamas <kitamas25@gmail.com>
2022-10-10 14:13:06 +0200

    commit

--- create.py
+++ create.py
@@ -160,10 +160,11 @@ def main():
     #end_event = datetime.datetime.strptime(event_result['end']['dateTime'],'%Y-%m-%dT%H:%M:%SZ')
     #print(end_event)
 
-    dd = datetime.datetime.fromisoformat('2012-11-01T04:16:13-04:00')
-    ddd = datetime.datetime(2012, 11, 1, 4, 16, 13, tzinfo=datetime.timezone(datetime.timedelta(days=-1, seconds=72000)))
+    dd = datetime.datetime.fromisoformat('2022-10-10T14:10:00+02:00')
+    ddd = datetime.datetime(2012, 11, 1, 4, 16, 13, tzinfo=datetime.timezone(datetime.timedelta(seconds=7200)))
     print("DD")
     print(dd)
+    print("DDD")
     print(ddd)
 
     text_param = {}
