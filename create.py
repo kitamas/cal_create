8f1d3ed9ce6eebe996e9345ae35f28c2e122f314
kitamas <kitamas25@gmail.com>
2022-10-10 17:44:33 +0200

    commit

--- create.py
+++ create.py
@@ -168,9 +168,9 @@ def main():
     return text_param
 
 
-def hour_rounder(t):
+#def hour_rounder(t):
     # Rounds to nearest hour by adding a timedelta hour if minute >= 30
-    return (t.replace(second=0, microsecond=0, minute=0, hour=t.hour) + datetime.timedelta(hours=t.minute // 30))
+    #return (t.replace(second=0, microsecond=0, minute=0, hour=t.hour) + datetime.timedelta(hours=t.minute // 30))
 
 def check_open():
     current_dateTime = datetime.datetime.now() + datetime.timedelta(hours=2)
