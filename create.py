1388136c232e0cfc3a33fbbee1349f7329808f69
kitamas <kitamas25@gmail.com>
2022-10-11 11:55:00 +0200

    commit

--- create.py
+++ create.py
@@ -21,6 +21,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 # QUICKSTART = = = = = = = = = = =
 
+import locale
 
 # Flask app should start in global layout
 app = flask.Flask(__name__)
@@ -192,6 +193,9 @@ def check_open():
 
     week_day_name = weekDays[week_day]
     print("week_day_name:", week_day_name)
+
+    locale.setlocale(locale.LC_TIME, "HU_hu.UTF8")
+    print("LOCALE LOCALE",current_dateTime.strftime('%A, %a, %B, %b'))
 
     print("open_start_time:", open_start_time[week_day])
     print("open_end_time:", open_end_time[week_day])
