15494caadcc1dd2955e86b9ef88d4ce3a3690ac6
kitamas <kitamas25@gmail.com>
2022-10-11 12:20:27 +0200

    commit

--- create.py
+++ create.py
@@ -194,8 +194,8 @@ def check_open():
     week_day_name = weekDays[week_day]
     print("week_day_name:", week_day_name)
 
-    locale.setlocale(locale.LC_ALL, "HU_hu.UTF8")
-    #locale.setlocale(locale.LC_TIME, "HU_hu.UTF8")
+    locale.setlocale(locale.LC_ALL, "HU_hu.utf8")
+    #locale.setlocale(locale.LC_TIME, "HU_hu.utf8")
     print("LOCALE LOCALE",current_dateTime.strftime('%A, %a, %B, %b'))
 
     print("open_start_time:", open_start_time[week_day])
