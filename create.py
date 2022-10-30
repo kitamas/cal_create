8748f7477b15bcbb0917d02034e9c03fe25b0e3d
kitamas <kitamas25@gmail.com>
2022-10-30 10:04:03 +0100

    commit

--- create.py
+++ create.py
@@ -222,7 +222,7 @@ def check_wd_open():
     print("open_end_time:", open_end_time[dt_p_week_day])
 
     print("AAAAAAAAAAAAAAAAAA hour_rounded = ", hour_rounded, type(hour_rounded), "open start time = ",open_start_time[dt_p_week_day],type(open_start_time[dt_p_week_day]))
-    print("BBBBBB hour_rounded = ", hour_rounded, type(hour_rounded), "open start time = ",datetime.strptime(open_start_time[dt_p_week_day],%H),type(datetime.strptime(open_start_time[dt_p_week_day])))
+    print("BBBBBB hour_rounded = ", hour_rounded, type(hour_rounded), "open start time = ",datetime.strptime(open_start_time[dt_p_week_day],%H) )
 
     if hour_rounded < open_start_time[dt_p_week_day]:
         print("KORÁN", hour_rounded, "<", open_start_time[dt_p_week_day])
