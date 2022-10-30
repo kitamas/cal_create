17eda989d971cc949686d3e9d844296b7f951d5f
kitamas <kitamas25@gmail.com>
2022-10-30 10:58:07 +0100

    commit

--- create.py
+++ create.py
@@ -228,11 +228,11 @@ def check_wd_open():
 
     print("hour_rounded.strftime = ",hour_rounded.strftime("%H"))
     print("int hour_rounded.strftime = ",int(hour_rounded.strftime("%H")))
-    print("IIIIIIIIINT, IIIIINT",int(hour_rounded.strftime("%H)), int(open_start_time[dt_p_week_day]))
+    print("IIIIIIIIINT, IIIIINT",int(hour_rounded.strftime("%H")), int(open_start_time[dt_p_week_day]))
 
 
-    #vv = datetime.time.strptime(open_start_time[dt_p_week_day],%H:%M)
-    #print("VVVVVV =",vv,type(vv))
+    vv = datetime.time.strptime(open_start_time[dt_p_week_day],"%H:%M")
+    print("VV VV VV =",vv,type(vv))
 
     if hour_rounded < open_start_time[dt_p_week_day]:
         print("KORÁN", hour_rounded, "<", open_start_time[dt_p_week_day])
