1df4a8105b2f2aee4438946ad14a2887e03e44dc
kitamas <kitamas25@gmail.com>
2022-11-01 08:26:57 +0100

    commit

--- create.py
+++ create.py
@@ -287,8 +287,8 @@ def get_events(start_p):
         print('START_P = ',start_p,type(start_p))
 
         print('Getting the upcoming 10 events')
-        #events_result = service.events().list(calendarId='61u5i3fkss34a4t50vr1j5l7e4@group.calendar.google.com', timeMin=now,
-        events_result = service.events().list(calendarId='61u5i3fkss34a4t50vr1j5l7e4@group.calendar.google.com', timeMin=start_p,
+        events_result = service.events().list(calendarId='61u5i3fkss34a4t50vr1j5l7e4@group.calendar.google.com', timeMin=now,
+        #events_result = service.events().list(calendarId='61u5i3fkss34a4t50vr1j5l7e4@group.calendar.google.com', timeMin=start_p,
                                               maxResults=10, singleEvents=True,
                                               orderBy='startTime').execute()
         events = events_result.get('items', [])
