1755e2c1650b0db774c857a4e94b0ae16546f24d
kitamas <kitamas25@gmail.com>
2022-11-01 08:02:56 +0100

    commit

--- create.py
+++ create.py
@@ -274,8 +274,36 @@ def check_wd_open():
 
 
 def get_events(start_p,end_p):
-   get_events_ret = "get_events_ret"
-   return get_events_ret
+    try:
+        service = build('calendar', 'v3', credentials=authentication())
+
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        # now = 2022-10-09T05:53:52.400939Z
+
+        print('Getting the upcoming 10 events')
+        events_result = service.events().list(calendarId='61u5i3fkss34a4t50vr1j5l7e4@group.calendar.google.com', timeMin=now,
+                                              maxResults=10, singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+            return
+
+        # Prints the start and name of the next 10 events
+        start_event = "" 
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            #print(start, event['summary'])
+            #start_event += start_event + " | " + event['summary'] + " | " + start
+            start_event += event['summary'] + " "  + start + " | "
+            #return event['summary']
+
+        return start_event
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
 
 
 def findFirstOpenSlot(events,startTime,endTime,duration):
