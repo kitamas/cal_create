299969bb10132293ca9ca62c24b0ed08e77ef218
kitamas <kitamas25@gmail.com>
2022-11-07 16:32:18 +0100

    commit

--- create.py
+++ create.py
@@ -329,7 +329,8 @@ def get_events(dt_p_obj_rounded,duration):
         start1 = events[0]['start'].get('dateTime', events[0]['start'].get('date'))
         start2 = datetime.datetime.strptime(start1,'%Y-%m-%dT%H:%M:%S%z')
         start = start2.strftime("%B %A %H:%M")
-
+        print("AAAAAAAA",start1,start2,start)
+        print("AAAAAAAA",events[0]['summary'])
         start_event += events[0]['summary'] + " "  + start + " | "
 
         return start_event
