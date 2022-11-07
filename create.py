fab3e429568e455039137230ce6e36c268970be3
kitamas <kitamas25@gmail.com>
2022-11-07 16:54:29 +0100

    commit

--- create.py
+++ create.py
@@ -278,7 +278,7 @@ def check_wd_open():
         print("hour rounded + duration = ",dt_p_obj_rounded + duration)
         print(" KOZOTTE", open_start_time[dt_p_week_day], "<=", dt_p_obj_rounded + duration, "<=", open_end_time[dt_p_week_day])
         #check_wd_open_text = open_start_time[dt_p_week_day] + " <= " + dt_p_obj_rounded.strftime("%B %A %H:%M") + " <= " + open_end_time[dt_p_week_day]
-        check_wd_open_text = open_start_time[dt_p_week_day] + " <= " + dt_p_obj_rounded.strftime(%H:%M") + " <= " + open_end_time[dt_p_week_day]
+        check_wd_open_text = open_start_time[dt_p_week_day] + " <= " + dt_p_obj_rounded.strftime("%H:%M") + " <= " + open_end_time[dt_p_week_day]
         boolean_wd_open = True
 
     check_wd_open_ret = [start_p,end_p,summary,location,check_wd_open_text,boolean_wd_open,dt_p_obj_rounded,duration,hours_am] 
