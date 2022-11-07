21faa79244d9131ce6d8abd7f55bcae3237c3424
kitamas <kitamas25@gmail.com>
2022-11-07 14:59:27 +0100

    commit

--- create.py
+++ create.py
@@ -294,14 +294,15 @@ def get_events(dt_p_obj_rounded,duration):
         # HOUR:00 - min1 = (HOUR-1):59   
 
         min1 = datetime.timedelta(minutes=1)
-
+        min11 = datetime.timedelta(minutes=11)
         start_p_min1 = (dt_p_obj_rounded - min1).isoformat("T", "seconds")
 
         start_p = start_p_min1 + '+00:00'
  
         print("GET EVENTS START P = ",start_p)
 
-        end_p1 = (dt_p_obj_rounded + duration - min1).isoformat("T", "seconds")
+        #end_p1 = (dt_p_obj_rounded + duration - min1).isoformat("T", "seconds")
+        end_p1 = (dt_p_obj_rounded + min11).isoformat("T", "seconds")
         end_p = end_p1 + '+00:00'
         print("GET EVENTS END P = ",end_p)
 
