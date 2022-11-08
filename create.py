245fa14c35056a841b5f27d2d347b14814571710
kitamas <kitamas25@gmail.com>
2022-11-08 10:26:25 +0100

    commit

--- create.py
+++ create.py
@@ -96,9 +96,10 @@ def webhook():
         main_ret =  main(start_p,end_p,summary,location)    
 
     #text = main_ret['text'] + check_wd_open_ret[4] + " B_1wd= " + str(check_wd_open_ret[5]) + " | " + get_events_ret + " | B_ev= " + str(boolean_get_events) + " hours_am:" + str(hours_am)
-    main_ret['text'] = "AA"
+    main_ret['text'] = "NO"
     text = main_ret['text'] + check_wd_open_ret[4] + " | " + get_events_ret
 
+    main_ret['event_id'] = ""
     event_id = main_ret['event_id']
 
     res = {
