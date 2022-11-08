f72036549a035f5815adc02cb869486c490742e3
kitamas <kitamas25@gmail.com>
2022-11-08 09:16:43 +0100

    commit

--- create.py
+++ create.py
@@ -301,8 +301,10 @@ def get_events(dt_p_obj_rounded,duration):
         start_p = start_p_min1 + '+00:00'
         print("GET EVENTS START P = ",start_p)
 
-        #end_p1 = (dt_p_obj_rounded + min1).isoformat("T", "seconds")
-        end_p1 = (dt_p_obj_rounded + duration - min1).isoformat("T", "seconds")
+        end_p1 = (dt_p_obj_rounded + min1).isoformat("T", "seconds")
+        #end_p1 = (dt_p_obj_rounded + duration - min1).isoformat("T", "seconds")
+        # lists ALL !!!
+
         end_p = end_p1 + '+00:00'
         print("GET EVENTS END P = ",end_p)
 
