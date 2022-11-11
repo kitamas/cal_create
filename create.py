6aceba10e958278287c94d99207e93441c10f2a4
kitamas <kitamas25@gmail.com>
2022-11-11 09:20:25 +0100

    commit

--- create.py
+++ create.py
@@ -331,7 +331,7 @@ def get_events(dt_p_obj_rounded,duration):
         startTime = dt_p_obj_rounded
         print("startTime = = =",startTime)
 
-        endTime = datetime.datetime(2022, 11, 10, 23, 59, 59, 0)
+        endTime = datetime.datetime(2022, 11, 11, 23, 59, 59, 0)
 
         duration = datetime.timedelta(hours = 1)
 
