58868e784e67cccbbfe9ddcdaf10777f3970da86
kitamas <kitamas25@gmail.com>
2022-11-16 09:06:39 +0100

    commit

--- create.py
+++ create.py
@@ -221,7 +221,7 @@ def check_wd_open():
     summary = req.get('sessionInfo').get('parameters').get('summary')
     location = req.get('sessionInfo').get('parameters').get('location')
 
-    open_start_time = ["08:00", "11:00", "10:00", "11:00", "08:00", "13:00", "11:00"]
+    open_start_time = ["08:00", "10:00", "11:00", "11:00", "08:00", "13:00", "11:00"]
     open_end_time = ["17:00", "19:00", "19:00", "18:00", "17:00", "19:00", "19:00"]
 
     week_days = ("hétfő", "kedd", "szerda", "csütörtök", "péntek", "szombat", "vasárnap")
